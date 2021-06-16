fa324d76772f9f1db65b301036e4c3e5daf2de83
Vasya <01vasil@mail.ru>
2021-06-16 23:46:50 +0300

    block color normalized

--- paperNormalize/index.js
+++ paperNormalize/index.js
@@ -68,27 +68,36 @@ let main = async ()=>{
         let maxB = Math.maxIndex(hB);
 
 
-        let bW = Math.ceil(info.width/128)
-        let bH = Math.ceil(info.height/128)
+        let blockSize = Math.ceil(Math.min(info.width,info.height )/8);
+        let bW = Math.ceil(info.width/blockSize);
+        let bH = Math.ceil(info.height/blockSize);
         let blocks=new Uint8Array(bW*bH);
+        let blockLights=new Uint8Array(bW*bH*3);
+        let blockDarks=new Uint8Array(bW*bH*3);
         //самые темные пиксели для контраста
         let minV = [999,999,999]
-        //изображение делим на блоки по 128х128 и для каждого блока вычисляем гистограмму
-        for(let y =0;y<info.height/128;y++){
-            for(let x =0;x<info.width/128;x++){
+        //изображение делим на блоки по blockSizeхblockSize и для каждого блока вычисляем гистограмму
+        for(let y =0;y<bH;y++){
+            for(let x =0;x<bW;x++){
                 let block = [];
-                for(let dy =0;dy<128;dy++){
-                    for(let dx =0;dx<128;dx++){
-                        block[dy*128*3+dx*3+0]=data[(y*128+dy)*info.width*3+(x*128+dx)*3+0];
-                        block[dy*128*3+dx*3+1]=data[(y*128+dy)*info.width*3+(x*128+dx)*3+1];
-                        block[dy*128*3+dx*3+2]=data[(y*128+dy)*info.width*3+(x*128+dx)*3+2];
+                for(let dy =0;dy<blockSize;dy++){
+                    for(let dx =0;dx<blockSize;dx++){
+                        block[dy*blockSize*3+dx*3+0]=data[(y*blockSize+dy)*info.width*3+(x*blockSize+dx)*3+0];
+                        block[dy*blockSize*3+dx*3+1]=data[(y*blockSize+dy)*info.width*3+(x*blockSize+dx)*3+1];
+                        block[dy*blockSize*3+dx*3+2]=data[(y*blockSize+dy)*info.width*3+(x*blockSize+dx)*3+2];
                     }
                 }
                 let histogram = getHistogram(block)
                 let maxBlockR = Math.maxIndex(histogram[0]);
                 let maxBlockG = Math.maxIndex(histogram[1]);
                 let maxblockB = Math.maxIndex(histogram[2]);
+                blockLights[(y*bW+x)*3+0] = maxBlockR;
+                blockLights[(y*bW+x)*3+1] = maxBlockG;
+                blockLights[(y*bW+x)*3+2] = maxblockB;
 
+                blockDarks[(y*bW+x)*3+0] = histogram[0].findIndex(v=>v>1)
+                blockDarks[(y*bW+x)*3+1] = histogram[1].findIndex(v=>v>1)
+                blockDarks[(y*bW+x)*3+2] = histogram[2].findIndex(v=>v>1)
 
                 
                 if(Math.abs(maxBlockR-maxR)>16||Math.abs(maxBlockG-maxG)>16||Math.abs(maxblockB-maxB)>16){
@@ -97,9 +106,9 @@ let main = async ()=>{
                 }else{
                     blocks[y*bW+x]=1;
                     //если в блоке преобладает бумага, то записываем самый темный уровень
-                    minV[0]=Math.min(minV[0], histogram[0].findIndex(v=>v>0))
-                    minV[1]=Math.min(minV[1], histogram[1].findIndex(v=>v>0))
-                    minV[2]=Math.min(minV[2], histogram[2].findIndex(v=>v>0))
+                    minV[0]=Math.min(minV[0], histogram[0].findIndex(v=>v>1))
+                    minV[1]=Math.min(minV[1], histogram[1].findIndex(v=>v>1))
+                    minV[2]=Math.min(minV[2], histogram[2].findIndex(v=>v>1))
                 }
             }
         }
@@ -125,20 +134,26 @@ let main = async ()=>{
         hG = hG.slice(32)
         hB = hB.slice(32)
 
+        let blockLightsResized = await sharp(blockLights.map(v=>v*4+4),{raw:{width:bW, height:bH, channels:3}})
+        .resize(info.width,info.height).toBuffer()
+        let blockDarksResized = await sharp(blockDarks.map(v=>v*4),{raw:{width:bW, height:bH, channels:3}})
+        .resize(info.width,info.height).toBuffer()
         //находим контраст по самой светлой части и самой темной
-        let lightR = 255/((Math.maxIndex(hR)-minV[0]+38)*4)
-        let lightG = 255/((Math.maxIndex(hG)-minV[1]+38)*4)
-        let lightB = 255/((Math.maxIndex(hB)-minV[2]+38)*4)
+        
 
         //применяем контраст
         for(let p=0;p!==data.length/3;p++){
-            
-            data[p*3+0]=Math.clamp((data[p*3+0]*lightR)-minV[0]*lightR,0,255);
-            data[p*3+1]=Math.clamp((data[p*3+1]*lightG)-minV[1]*lightG,0,255);
-            data[p*3+2]=Math.clamp((data[p*3+2]*lightB)-minV[2]*lightB,0,255);
+            let lightR = 255/(blockLightsResized[p*3+0]-minV[0])//-minV[0]*4)
+            let lightG = 255/(blockLightsResized[p*3+1]-minV[1])//-minV[1]*4)
+            let lightB = 255/(blockLightsResized[p*3+2]-minV[2])//-minV[2]*4)
+            data[p*3+0]=Math.clamp((data[p*3+0]-minV[0])*lightR,0,255);
+            data[p*3+1]=Math.clamp((data[p*3+1]-minV[1])*lightG,0,255);
+            data[p*3+2]=Math.clamp((data[p*3+2]-minV[2])*lightB,0,255);
         }
+        await sharp(blockLightsResized,{raw:{width:info.width, height:info.height, channels:3}})
+        .png().toFile('./normalizedFiles/'+ fileName.slice(0, -4)+'small.png')
         await sharp(data,{raw:{width: info.width,height: info.height,channels:3}})
-            .extract({top:minY*128,left:0, width:info.width, height:Math.min(info.height-minY*128,(maxY - minY)*128)})
+            .extract({top:minY*blockSize,left:0, width:info.width, height:Math.min(info.height-minY*blockSize,(maxY - minY)*blockSize)})
             .png().toFile('./normalizedFiles/'+ fileName.slice(0, -4)+'.png');
     }
 
