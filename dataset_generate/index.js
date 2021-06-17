85d70a17829918fdc8f298bfaae9b596328567c8
vasyainformatica <01vasil@mail.ru>
2021-06-17 10:21:28 +0300

    generate lines in dataset

--- dataset_generate/index.js
+++ dataset_generate/index.js
@@ -18,6 +18,14 @@ module.exports = async (quality = 60)=>{
         background: { r: light-temperature, g: light, b: light+temperature, alpha: 1 }
         }
     })
+    let lineImage = sharp({
+        create: {
+        width: w,
+        height: h,
+        channels: 3,
+        background: { r: 0, g: 0, b: 0, alpha: 1 }
+        }
+    })
     let degree = Math.random()*20-10;
     
 
@@ -27,24 +35,47 @@ module.exports = async (quality = 60)=>{
     let width = 100;
 
     let texts = []
+    let lines = []
     for(let y = 0; y< h;y+=60){
 
         for(let x = 50; x< w-150;x+=width){
             text = (new Array(Math.floor(Math.random()*10)).fill(0).map(v=>alphabet[Math.floor(Math.random()*alphabet.length)])).join('')+' '
 
             width = text.length*16
-            texts.push(`<text x="${x}" y="${y}" font-size="24px" fill="#000">`+text+`</text>`)
+            texts.push(`<text x="${x}" y="${y}" style="Rrrrr">`+text+`</text>`)
         }
+        let lineOffset = Math.floor(Math.random()*30)
+        lines.push(`<line x1="${50}" y1="${y+10+lineOffset}" x2="${w-150}" y2="${y+10+lineOffset}" stroke="black" />`)
     }        
     const textedSVG = Buffer.from(`<svg x="0px" y="0px" width="${w}px" height="${h}px" viewBox="0 0 ${w} ${h}">
             <style>
-            .Rrrrr { font: italic 24px serif; fill: black; }
+            text { font: italic 24px serif; fill: black; }
           </style>
                 ${texts.join('')}
-            </svg>`)
-            origin = await origin.composite([{input:textedSVG}])
-            origin = sharp(await origin.toBuffer(),{raw:{width:w, height:h, channels:4}})
+            </svg>`);
+    const linedSVG = Buffer.from(`<svg x="0px" y="0px" width="${w}px" height="${h}px" viewBox="0 0 ${w} ${h}">
+            <style>
+          </style>
+                ${lines.join('')}
+            </svg>`);        
+
     
+    lineImage = await lineImage.composite([{input:Buffer.from(`<svg x="0px" y="0px" width="${w}px" height="${h}px" viewBox="0 0 ${w} ${h}">
+    <style>
+    line { stroke: #fff; }
+  </style>
+        ${lines.join('')}
+    </svg>`)}])
+    origin = await origin.composite([{input:textedSVG}, {input:Buffer.from(`<svg x="0px" y="0px" width="${w}px" height="${h}px" viewBox="0 0 ${w} ${h}">
+    <style>
+    line { stroke: #000; }
+  </style>
+        ${lines.join('')}
+    </svg>`)}])
+    lineImage = sharp(await lineImage.toBuffer(),{raw:{width:w, height:h, channels:4}})
+    origin = sharp(await origin.toBuffer(),{raw:{width:w, height:h, channels:4}})
+    
+    lineImage = sharp(await lineImage.toBuffer(),{raw:{width:w, height:h, channels:4}}).rotate(degree).extract({left:0, top:0, width:w, height:h})
     origin = sharp(await origin.toBuffer(),{raw:{width:w, height:h, channels:4}}).rotate(degree).extract({left:0, top:0, width:w, height:h})
     let originData = await origin.clone().extractChannel(0).toColorspace('b-w').raw().toBuffer()
 
@@ -53,8 +84,13 @@ module.exports = async (quality = 60)=>{
     let out ={ width:w, height:h};
 
     let resized = await origin.resize(wh,hh).jpeg({quality}).toBuffer()
+    out.lines = await lineImage.resize(wh, hh).extractChannel(0).toColorspace('b-w').threshold(64).toBuffer()
     out.result = await sharp(resized).raw().extractChannel(0).toColorspace('b-w').toBuffer()
     out.origin = originData;
 
     return out
 }
+// module.exports().then((res)=>{
+//     console.log(res.result.length, res.width*res.height );
+//     sharp(res.lines,{raw:{width:res.width/2,height:res.height/2, channels:1}}).png().toFile('test.png')
+// })
--- dataset_generate/test.js
+++ dataset_generate/test.js
@@ -7,11 +7,11 @@ let main = async ()=>{
 
     let model = await tf.loadLayersModel('file://model/model.json');
     let img = tf.node.decodeJpeg(fs.readFileSync('img.jpg'), 1).expandDims()
-    let result = model.predict(img.div(255))
+    let result = model.predict(img.div(100))
 
-    let y = result.add(img.div(255).resizeBilinear([img.shape[1]*2, img.shape[2]*2]))
+    let y = result.add(img.div(100).resizeBilinear([img.shape[1]*2, img.shape[2]*2]))
     
-    let buf = await tf.node.encodePng(y.reshape(y.shape.slice(1)).mul(255))
+    let buf = await tf.node.encodePng(y.reshape(y.shape.slice(1)).mul(100))
     console.log('buf: ', buf);
     fs.writeFileSync('./res.png', buf)
 }
